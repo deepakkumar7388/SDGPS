646a2691b04398062885029fb5c7d7983ab0bd40
Yogesh Saini <yogeshsaini7172@gmail.com>
2026-07-25 22:18:05 +0530

    implementation of show and hide password

--- web/vite.config.js
+++ web/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        // target: 'https://digitalpassbackend-dmwz.onrender.com',
-        target: 'http://10.222.29.1:5000',
+        target: 'https://digitalpassbackend-dmwz.onrender.com',
+        // target: 'http://10.222.29.1:5000',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
